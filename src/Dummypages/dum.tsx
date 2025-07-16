8cb47a5c15f8abdecbd9efa7c9807b6a14966201
kakugb <karamat.4101@gmail.com>
2025-07-16 12:33:22 -0700

    add ui components

---
+++ src/Dummypages/dum.tsx
@@ -0,0 +1,23 @@
+// import i18n from 'i18next';
+// import { initReactI18next } from 'react-i18next';
+// import LanguageDetector from 'i18next-browser-languagedetector';
+// import HttpApi from 'i18next-http-backend';
+
+// i18n
+//   .use(HttpApi)
+//   .use(LanguageDetector) // No custom detectors
+//   .use(initReactI18next)
+//   .init({
+//     supportedLngs: ['en', 'de', 'id', 'uk', 'ko', 'ja', 'pl', 'ar', 'nl', 'tr', 'pt', 'it', 'es', 'fr'],
+//     fallbackLng: 'en',
+//     detection: {
+//       order: ['querystring', 'cookie', 'localStorage', 'navigator', 'htmlTag'],
+//       caches: ['cookie'],
+//     },
+//     backend: {
+//       loadPath: '/locales/{{lng}}/{{ns}}.json',
+//     },
+//     debug: true,
+//   });
+
+// export default i18n;
